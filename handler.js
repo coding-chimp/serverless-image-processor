99b17a356a83a7d2e1e37718d8883a762e4ab08c
coding-chimp <xarfai27@gmail.com>
2018-03-22 21:40:01 +0100

    Change path and let image have a proper name

--- handler.js
+++ handler.js
@@ -1,12 +1,13 @@
 const Sharp = require('sharp');
+const Path = require('path');
 const ImageFetcher = require('./src/image-fetcher');
 const ImageProcessor = require('./src/image-processor');
 
 module.exports.processImage = (event, context, callback) => {
-  const bucket = event.queryStringParameters && event.queryStringParameters.bucket;
+  const bucket = decodeURIComponent(event.pathParameters.bucket);
   const imageFetcher = new ImageFetcher(bucket);
   const imageProcessor = new ImageProcessor(Sharp);
-  const fileName = event.queryStringParameters && event.queryStringParameters.file;
+  const file = decodeURIComponent(event.pathParameters.file);
   const quality = event.queryStringParameters && +event.queryStringParameters.quality || 100;
   const size = {
     width: event.queryStringParameters && +event.queryStringParameters.width || null,
@@ -15,16 +16,20 @@ module.exports.processImage = (event, context, callback) => {
   const blur = event.queryStringParameters && +event.queryStringParameters.blur || false;
 
   return imageFetcher
-    .fetchImage(fileName)
+    .fetchImage(file)
     .then(data => imageProcessor.resize(data.image, size, quality))
     .then(data => blur ? imageProcessor.blur(data.image, blur) : data)
     .then(data => {
       const contentType = data.contentType;
       const img = new Buffer(data.image.buffer, 'base64');
+      const fileName = Path.parse(file).base;
 
       callback(null, {
         statusCode: 200,
-        headers: { 'Content-Type': contentType },
+        headers: {
+          'Content-Type': contentType,
+          'Content-Disposition': 'inline; filename="' +  fileName + '"'
+        },
         body: img.toString('base64'),
         isBase64Encoded: true,
       });
